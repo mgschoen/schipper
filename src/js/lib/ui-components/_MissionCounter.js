2b43d57f31ee285a7a9ef7ecb1b7440fccc99fea
Martin Schön <martin.schoen@spiegel.de>
2021-05-04 20:51:11 +0200

    Added MissionCounter component

---
+++ src/js/lib/ui-components/_MissionCounter.js
@@ -0,0 +1,55 @@
+import AbstractComponent from './_AbstractComponent';
+import { useStore } from '../store';
+
+export default class MissionCounter extends AbstractComponent {
+    constructor(...args) {
+        super(...args);
+
+        this._visible = false;
+
+        this.storeSubscription = useStore([
+            'missionLifecycle',
+            'missionIndexCurrent',
+            'missionCountTotal'
+        ], (state) => this.onStoreChanged(state));
+    }
+
+    onStoreChanged(state) {
+        this.visible = state.missionLifecycle !== 'inactive';
+        console.log(state);
+        console.log(this.elements);
+        if (typeof state.missionIndexCurrent === 'number') {
+            this.elements.current.textContent = state.missionIndexCurrent + 1;
+        }
+        if (typeof state.missionCountTotal === 'number') {
+            this.elements.total.textContent = state.missionCountTotal;
+        }
+    }
+
+    updateVisibility() {
+        if (this.visible) {
+            this.root.style.opacity = '1';
+            this.root.style.transform = 'translateY(0)';
+        } else {
+            this.root.style.opacity = '0';
+            this.root.style.transform = 'translateY(-150%)';
+        }
+    }
+
+    get visible() {
+        return this._visible;
+    }
+
+    set visible(value) {
+        if (!typeof value === 'boolean') {
+            return;
+        }
+        this._visible = value;
+        this.updateVisibility();
+    }
+
+    destroy() {
+        super.destroy();
+        this.storeSubscription.cancel();
+    }
+}
--- src/js/lib/ui-components/index.js
+++ src/js/lib/ui-components/index.js
@@ -1,6 +1,8 @@
 import ComponentLoader from './_ComponentLoader';
 import MissionDashboard from './_MissionDashboard';
+import MissionCounter from './_MissionCounter';
 
 export default () => new ComponentLoader({
+    MissionCounter,
     MissionDashboard
 });
